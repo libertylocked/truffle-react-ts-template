c05aef70fd35e642c81edf963c78f2229388a4fb
libertylocked <unlock.liberty@gmail.com>
2017-11-12 07:08:49 -0500

    Clean up index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import web3 from "./global/web3client";
 
 require("./index.css");
-
-// expose web3 to console
-(window as any).web3 = web3;
 
 ReactDOM.render(
   <App />,
