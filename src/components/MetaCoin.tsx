547901f1e05cb6c37bdb21654e173e7c5c84b00c
libertylocked <unlock.liberty@gmail.com>
2017-11-16 11:24:28 -0500

    Add bignumber.js dependency

--- src/components/MetaCoin.tsx
+++ src/components/MetaCoin.tsx
@@ -1,4 +1,4 @@
-import BigNumber from "bignumber.js";
+import { BigNumber } from "bignumber.js";
 import * as React from "react";
 import * as TruffleContract from "truffle-contract";
 import * as Web3 from "web3";
